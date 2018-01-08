824f05cc18bec63c25fa5d5c40f8489e87714a6f
cuishiyao <cst_study@163.com>
2018-01-08 16:07:51 +0800

    [update]AS信息获取代码

--- IP_CAME/get_ip_asinfo.py
+++ IP_CAME/get_ip_asinfo.py
@@ -1,69 +1,145 @@
 #-*- coding:utf-8 -*-
 """
-    与whois.cymru.com建立连接获取AS信息
+    获取AS信息
+    注：
+        1. 循环获取时，每次修改last_visit_times的值
+        2. 循环获取时先测试一下
 """
-import socket
-import datetime
+
+"""数据库模块引入"""
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+sys.path.append("..") # 回退到上一级目录
+import database.mongo_operation
+mongo_conn = database.mongo_operation.MongoConn('172.29.152.152','mal_domain_profile')
 
-def get_asn_whois(query_ip):
-    '''
-    功能： 向whois.cymru.com发出查询，获得ip的AS、ASNAME和管理组织名称asn_registry
+"""IP AS获取引入"""
+import ASN.ip_as
 
-    return:完整的AS信息
-    '''
-    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    conn.settimeout(10)
-    conn.connect(('38.229.36.122', 43))
-    # Query the Cymru whois server, and store the results.
-    conn.send((
-        ' -r -a -c -p -f {0}{1}'.format(
-            query_ip, '\r\n')
-    ).encode())
+"""多线程相关"""
+import Queue
+import threading
+import time
 
-    data = ''
+"""线程数量"""
+thread_num = 2
+
+"""同步队列"""
+domain_q = Queue.Queue()
+res_q = Queue.Queue()
+
+"""库中visit_times对应的数值"""
+last_visit_times = 1
+
+def get_domains(limit_num = None):
+    """
+    从数据库中获取要初始获取数据的域名
+    注：1 last_visit_times 控制这是对第几次获取的数据获取AS信息
+       2 limit_num 控制是否获取一定量的域名
+    """
+    global mongo_conn
+    global last_visit_times
+    global domain_q
+    global res_q
+    global last_visit_times
+    cur_array = 'domain_ip_cnames.' + str(last_visit_times - 1) + '.ip_as'
+
+    fetch_data = mongo_conn.mongo_read('domain_ip_cname',{'visit_times':last_visit_times,
+                                                        cur_array:{'$exists':False}},
+                                                        {'domain':True,'domain_ip_cnames':True,'_id':False},limit_num
+                                        )
+    for item in fetch_data:
+        ips = item['domain_ip_cnames'][last_visit_times - 1]['ips'] # 获取上一次新插入的ip
+        if ips:
+            domain_q.put({item['domain']:ips})
+        else:
+            # ip序列为空的，as信息也空，直接存储结果
+            res_q.put([item['domain'],[]])
+
+
+def get_asinfo():
+    """
+        具体为每个域名所对应的ip获取as信息
+        param dm_ip_dict: {domain:[ip1,ip2, ..., ipn]}
+        return domain,[{ASN:'',ASOWNER:'',...,},{ASN:'',ASOWNER:'',...,},...,{ASN:'',ASOWNER:'',...,}]
+    """
+    global domain_q
+    global res_q
+
+    while not domain_q.empty():
+        dm_ip_dict = domain_q.get()
+        flag = True # 获取as未出现异常的标志
+
+        domain = dm_ip_dict.keys()[0]
+        dm_ip_asinfo = []
+        print '获取域名 ' + domain + ' 的IP信息...'
+
+        for ip in dm_ip_dict[domain]:
+            try:
+                std_asinfo = ASN.ip_as.get_std_asinfo(ip)
+                dm_ip_asinfo.append(std_asinfo)
+            except Exception, e:
+                # 出现异常则停止此域名的相关获取，否则会导致ip和as信息不对应
+                flag = False
+                break
+
+        # 未出现异常的as信息获取加入结果队列
+        if flag:
+            res_q.put([domain,dm_ip_asinfo])
+    print 'as信息获取完成...'
+
+
+def save_asinfo():
+    """
+    存储as信息
+    param: domain :域名
+    param: dm_ip_asinfo: [{ASN:'',ASOWNER:'',...,},{ASN:'',ASOWNER:'',...,},...,{ASN:'',ASOWNER:'',...,}]
+    """
+
+    global res_q
+    global last_visit_times
+    cur_array = 'domain_ip_cnames.' + str(last_visit_times - 1) + '.ip_as'
+
     while True:
-
-        d = conn.recv(4096).decode()
-        data += d
-
-        if not d:
-
+        try:
+            domain,dm_ip_asinfo = res_q.get(timeout=200)
+        except Queue.Empty:
+            print '存储完成'
             break
-    return data
+
+        try:
+            mongo_conn.mongo_update('domain_ip_cname',{'domain':domain},{cur_array:dm_ip_asinfo},multi_flag=True)
+            print domain + ' saved ...'
+        except Exception,e:
+            print domain + str(e)
+            continue
+
+    print '所有信息存储完成... '
 
 
 
-def parse_as_info(asn_whois):
-    '''
-    解析asn的信息
-    '''
-    temp = asn_whois.split('|')
-    parsed_as_info = {}
-    parsed_as_info['ASN'] = temp[0].strip(' \n')
-    parsed_as_info['AS_cidr'] = temp[2].strip(' \n')
-    parsed_as_info['AS_country_code'] = temp[3].strip(' \n').upper()
-    parsed_as_info['RIR'] = temp[4].strip(' \n')
-    parsed_as_info['AS_date'] = temp[5].strip(' \n')
-    parsed_as_info['AS_owner'] = temp[6].strip(' \n')
-    return parsed_as_info
-
-
-def get_std_asinfo(ip):
+def main():
     """
-    封装以上两个函数，获取完整as信息
+    集成以上内容的主函数
     """
-    as_info = get_asn_whois(ip)
-    std_asinfo = parse_as_info(as_info)
-    std_asinfo['AS_insert_time'] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return std_asinfo
+    print '获取域名...'
+    get_domains(10)
+    get_as_td = []
+    for _ in range(thread_num):
+        get_as_td.append(threading.Thread(target=get_asinfo))
+    for td in get_as_td:
+        td.start()
+    print 'getting as info ...\n'
+    time.sleep(10)
+    print 'save as info ...\n'
+    save_db_td = threading.Thread(target=save_asinfo)
+    save_db_td.start()
+    save_db_td.join()
 
 
 if __name__ == '__main__':
-    """
-    '38.229.36.122'
-    '36.0.0.9'
-    """
-    print get_std_asinfo('36.0.0.9')
+    main()
+    '''
+    # domain,dm_ip_asinfo = get_asinfo({'0-6-baby.com':["58.222.39.52"]})
+    # print domain,dm_ip_asinfo
+    # save_asinfo('0-6-baby.com',dm_ip_asinfo)
+    '''
