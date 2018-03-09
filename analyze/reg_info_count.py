0e081b391fee59e3f6b014c1848f3b580e758c10
cuishiyao <cst_study@163.com>
2018-03-09 17:12:31 +0800

    [update]修改注册信息统计方法

--- analyze/reg_info_count.py
+++ analyze/reg_info_count.py
@@ -14,61 +14,67 @@ import database.mysql_operation
 mysql_conn = database.mysql_operation.MysqlConn('172.26.253.3','root','platform','mal_domain_profile','utf8')
 
 
-def ini_reg_info_table(reg_type):
-    """
-    通过whois表统计，向reg_info表插入初始的数据（item,type,count）
-    # 无法跨表字段group by？
-    """
-    sql = "select %s,count(*) from domain_whois group by %s;" %(reg_type,reg_type)
-    for item,domain_count in fetch_data:
-        if item == '':
-            # 空的注册信息不进行统计
-            continue
-
-        # 对注册信息进行转义
-        item = MySQLdb.escape_string(item)
-        sql = "REPLACE INTO reg_info(item,reg_type,domain_count) VALUES('%s','%s',%d);" %(item,reg_type,domain_count)
-        exec_res = mysql_conn.exec_cudsql(sql)
-        # print item,domain_count
-    print '初始化完成...'
-    mysql_conn.commit()
-
-
 def type_count(reg_type):
     """
-    功能：统计每个注册信息下对应不同类型的域名数量
+    功能：统计每个注册信息(reg_name/reg_email/reg_phone/registrar)下对应不同类型的域名数量
+
+    虽然遍历了2遍数据，但是减少了数据库操作
     """
     counter = 0
     whois_reg_type = 'domain_whois.' + reg_type
     sql = "select domain_index.maltype,%s,count(*) from domain_index,domain_whois\
            where domain_index.domain = domain_whois.domain \
            group by domain_index.maltype,%s;" %(whois_reg_type,whois_reg_type)
+
+    reg_info_dict = {}
+
+    # 对分类统计信息进行整合
     fetch_data = mysql_conn.exec_readsql(sql)
     for item in fetch_data:
-        print item
+        # reg_item是具体的reg_email/reg_name/reg_phone/registrar
+        reg_item = item[1]
+        count = int(item[2])
+
+        if reg_item == '':
+            continue
+
+        if reg_item not in reg_info_dict:
+            reg_info_dict[reg_item] = {'gamble':0,'porno':0}
+
         if str(item[0]) == '非法赌博':
-            sql = "UPDATE reg_info SET gamble_count = %d WHERE item = '%s';" %(int(item[2]),item[1])
-        elif str(item[0]) == '色情':
-            sql = "UPDATE reg_info SET porno_count = %d WHERE item = '%s';" %(int(item[2]),item[1])
+            reg_info_dict[reg_item]['gamble'] = count
+        if str(item[0]) == '色情':
+            reg_info_dict[reg_item]['porno'] = count
+
+    for item in reg_info_dict:
+
+        gamble_count = reg_info_dict[item]['gamble']
+        porno_count = reg_info_dict[item]['porno']
+        domain_count = gamble_count + porno_count
+
+        # 对注册信息进行转义
+        reg_item = MySQLdb.escape_string(item)
+        print reg_item,reg_type,domain_count,gamble_count,porno_count
+
+        sql = "INSERT INTO reg_info_copy(item,reg_type,domain_count,gamble_count,porno_count)\
+               VALUES('%s','%s',%d,%d,%d)\
+               ON DUPLICATE KEY\
+               UPDATE domain_count=%d,gamble_count=%d,porno_count=%d;"%(reg_item,reg_type,domain_count,gamble_count,porno_count,domain_count,gamble_count,porno_count)
+
         exec_res = mysql_conn.exec_cudsql(sql)
         if exec_res:
             counter += 1
             if counter == 100:
                 mysql_conn.commit()
                 counter = 0
-    # 通过sql语句更新注册商对应的域名总量
-    # sql = "UPDATE domain_registrar SET domain_count = gamble_count + porno_count;"
-    # exec_res = mysql_conn.exec_cudsql(sql)
+    exec_res = mysql_conn.exec_cudsql(sql)
     mysql_conn.commit()
 
+
 def main():
-    for reg_type in ['reg_email','reg_name','reg_name']:
-        ini_reg_info_table(reg_type)
-        print reg_type + " 初始完成"
+    for reg_type in ['reg_email','reg_name','reg_name','sponsoring_registrar']:
         type_count(reg_type)
         print reg_type + " 统计完成"
-
-
 
 if __name__ == '__main__':
     main()
